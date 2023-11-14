60dd92538180f95eb345f9098613ff8702415e5c
Yuval Hadar <35yuval2@gmail.com>
2023-11-14 02:01:32 +0200

    changed gap size

--- src/Comps/Chart.tsx
+++ src/Comps/Chart.tsx
@@ -42,7 +42,7 @@ const Chart: React.FC<iChart> = ({title}) => {
 
       const LineProgress = ({value, color} : {value : number, color: tColor}) => (
         <div className="w-full bg-gray-200 rounded-full bg-gray-700 relative" style={{height: "1rem"}}>
-            
+    
             <div className={`rounded-full ${color === "success" ? "bg-success" : color === "warning" ? "bg-warning" : "bg-error"}`} style={{width: `${value}%`, height: "1rem"}} />
             <p className={"absolute inset-0 font-semibold text-white text-center leading-4"} style={{top: "1px"}} >{value}%</p>
         </div>
@@ -51,21 +51,21 @@ const Chart: React.FC<iChart> = ({title}) => {
       const CircularProgress = ({value, color} : {value : number, color: tColor}) => (
         <OnTop>
         <div className="radial-progress text-gray-700" style={{"--value": 100, "--thickness": "1rem", "--size": "180px",}} role="progressbar" />
-            <div className={`radial-progress z-10 ${color === "success" ? "text-success" : color === "warning" ? "text-warning" : "text-error"}`} style={{"--value": value, "--thickness": "1rem", "--size": "180px",}} role="progressbar">
+            <div className={`radial-progress ${color === "success" ? "text-success" : color === "warning" ? "text-warning" : "text-error"}`} style={{"--value": value, "--thickness": "1rem", "--size": "180px",}} role="progressbar">
                 <p className="text-white text-3xl">{value}%</p>
             </div>
         </OnTop>
       );
       
     return (
-        <Paper bgcolor="bg-base-200" classnames="w-80 flex flex-col justify-center items-center gap-3 p-3">
+        <Paper bgcolor="bg-base-200" classnames="w-64 flex flex-col justify-center items-center gap-2 py-3">
             <p className="text-center text-white text-2xl font-bold">{title}</p>
             <div className="collapse">
                 <input type="checkbox" />
                 <div className="collapse-title">
                     <CircularProgress value={value} color={color} />
                 </div>
-                <div className="collapse-content">
+                <div className="collapse-content flex flex-col gap-2">
                     <LineProgress value={value} color={color}/>
                     <LineProgress value={value} color={color}/>
                     <LineProgress value={value} color={color}/>
